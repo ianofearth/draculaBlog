faa8bbb39dc9848a4c387d07a0a53757cf9465b9
ian bevel <student@epicodus.com>
2015-06-08 16:24:54 -0700

    add stores.js with stored information, add sort posts by title, add edit comments functionality

---
+++ store.js
@@ -0,0 +1,50 @@
+var posts = [
+  {
+    id: '1',
+    title: "I love blood",
+    body: "I really really love blood.  I mean like alot."
+  },
+  {
+    id:'2',
+    title: "The Count",
+    body: "So the count is a dude.  He seems like kind of a creeper."
+  },
+  {
+    id: '3',
+    title: "Thievery",
+    body: "Stuff got stolen, it was a bummer."
+  },
+  {
+    id: '4',
+    title: "Another one",
+    body: "this is another to test the sort functionality."
+  }
+];
+
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'What a shame!'
+  },
+  {
+    id: '2',
+    postId: '3',
+    text: 'I am so sorry.  How are you feeling now?'
+  },
+  {
+    id: '3',
+    postId: '1',
+    text: 'I love blood too.'
+  },
+  {
+    id: '4',
+    postId: '1',
+    text: 'Blood is gross.'
+  },
+  {
+    id: '5',
+    postId: '1',
+    text: 'Crazy stuf man'
+  },
+];
