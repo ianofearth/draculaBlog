bce951fe325a27a200daf58741267c18f6a40c61
ian bevel & andrew lee <student@epicodus.com>
2015-06-09 09:07:29 -0700

    add note

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ Blogger.Router.map(function() {
   this.resource('posts', {path: '/'});
   this.resource('about');
   this.resource('contact', function() {
-    this.resource('phone');
+    this.resource('phone');  //nested route, uses {{outlet}} to display stuff that is nested
     this.resource('email');
   });
   this.resource('recent-comments');
